09f9dc400fbfa2fbb2d5c42b80b27254435934d1
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2025-01-07 11:09:08 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using CSHARP25010702;
+
+const string PRJDIR = "E:\\PROJECTS\\CSHARP25010702\\src";
+
+List<Tehen> happyCows = [];
+
+using StreamReader sr = new($"{PRJDIR}\\hozam.txt");
+while (!sr.EndOfStream)
+{
+    string[] adatSor = sr.ReadLine().Split(';');
+    string id = adatSor[0];
+    string nap = adatSor[1];
+    string mennyiseg = adatSor[2];
+
+    Tehen aktTehen = new(id);
+    if (!happyCows.Contains(aktTehen)) happyCows.Add(aktTehen);
+    int index = happyCows.IndexOf(aktTehen);
+    happyCows[index].EredmenytRogzit(nap, mennyiseg);
+}
+
+Console.WriteLine("3. feladat");
+Console.WriteLine($"Az állomány {happyCows.Count} tehén adatait tartalmazza.");
